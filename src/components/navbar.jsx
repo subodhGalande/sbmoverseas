724f38ddf1242379dd7f183049f7674ff0a5b752
subodh Galande <subodh.dev@outlook.com>
2022-09-19 01:50:15 +0530

    mobile navbar done

---
+++ src/components/navbar.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { ReactDOM } from "react";
+import sbmlogo from "../assets/SBM.png";
+import { Popover } from "@headlessui/react";
+import { NavLink } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+
+const Navbar = () => {
+  return (
+    <>
+      <header class="w-screen mt-7">
+        <nav class="  ">
+          <div class="flex mx-auto justify-between  w-full px-4 sm:px-8 lg:px-12">
+            <img src={sbmlogo} class="inline-block h-10" alt="" />
+            <ul class=" hidden sm:visible space-x-4">
+              <li class="inline-block">Home</li>
+              <li class="inline-block">Products</li>
+              <li class="inline-block">Contact Us</li>
+            </ul>
+            <div class="sm:hidden">
+              <Popover className="sm:hidden text-slate-900 py-1 px-2 rounded-md ring-1 ring-zinc-500/40 ">
+                <Popover.Button>
+                  {" "}
+                  <GiHamburgerMenu />{" "}
+                </Popover.Button>
+
+                <Popover.Panel className="absolute z-10">
+                  <div class="fixed grid inset-x-4 top-7 z-50 origin-top rounded-md p-8 ring-1 ring-zinc-500/40 bg-white opacity-100 scale-100">
+                    {" "}
+                    <div class="flex flex-row justify-between">
+                      <h6 class="inline-block text-slate-600">Menu</h6>
+                      <Popover.Button
+                        class="inline-block"
+                        aria-label="Close menu"
+                        type="button"
+                        data-headlessui-state="open"
+                        tabindex="0"
+                      >
+                        {" "}
+                        <MdClose />
+                      </Popover.Button>
+                    </div>
+                    <ul class=" mt-4 divide-y font-sans text-base font-normal text-slate-900 divide-zinc-500/40 ">
+                      <li>
+                        {" "}
+                        <Popover.Button class="pt-3 pb-2">About</Popover.Button>
+                      </li>
+                      <li>
+                        <Popover.Button class="pt-3 pb-2">
+                          Projects
+                        </Popover.Button>
+                      </li>
+                      <li>
+                        <Popover.Button class="pt-3 pb-2">Blog</Popover.Button>
+                      </li>
+                    </ul>
+                  </div>
+                </Popover.Panel>
+              </Popover>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
