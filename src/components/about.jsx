9dbbae2339838c2992127048c22273448e320aec
subodh Galande <subodh.dev@outlook.com>
2022-09-19 21:23:32 +0530

    about us complete

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { ReactDOM } from "react";
-import about from "../assets/about.png";
 
 const About = () => {
   return (
     <>
-      <section class="mt-32 px-4 md:px-8 2xl:px-12">
-        <h1 className=" mx-auto text-transparent text-5xl bg-clip-text bg-gradient-to-r from-blue-600 via-blue-400 to-blue-600 lg:text-7xl font-serif  md:text-8xl md:text-left text-center font-bold">
+      <div class="container mx-auto max-w-5/6 mt-20 px-4 md:px-8 lg:px-12">
+        <h1 className=" mx-auto font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-blue-600 via-blue-400 to-blue-600  lg:text-7xl font-serif md:text-5xl text-center ">
           About Us
         </h1>
-        <div class="flex flex-col mt-8 ">
-          {/* <div class="inline-block mx-auto h-48 w-48 ">
-            <img src={about} alt="" />
-          </div> */}
-          <div class="inline-block space-y-3  text-slate-600 text-base md:text-xs xl:text-lg text-center md:text-left lg:text-base mt-6 lg:mt-10 font-medium">
+
+        <h3 className=" text-center  mx-auto  leading-tight text-slate-600 text-lg md:text-md xl:text-2xl mt-8 md:mt-6 lg:mt-5 font-bold">
+          <em class="text-semibold">SBM Overseas </em>is having its own roots at
+          all stages results in competitive price and quality excellence.
+        </h3>
+        <div class="container mx-auto w-full h-full sm:space-x-10 mt-5 lg:w-4/5 ">
+          <div class=" space-y-3  text-slate-500 text-base md:text-sm xl:text-lg text-center lg:text-base mt-10 lg:mt-10 font-medium">
             <p>
-              The company is having its own roots at all stages results in
-              competitive price and quality excellence. The company is having
-              all in one roof facility i. e. trading, processing, storing, local
-              transportation etc. helps to cut cost and value to its product.
-              The all in one roof facility helps it to attain core competence in
-              the market.
+              The company is having all in one roof facility i. e. trading,
+              processing, storing, local transportation etc. helps to cut cost
+              and value to its product. The all in one roof facility helps it to
+              attain core competence in the market.
             </p>
             <p>
               The company is backed by solid Information Technology and using
@@ -40,7 +39,7 @@ const About = () => {
             </p>
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
