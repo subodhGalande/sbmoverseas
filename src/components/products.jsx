2af1f9bd65609f4b7ed84beb227225ea98e33ab1
subodh Galande <subodh.dev@outlook.com>
2022-09-23 15:40:05 +0530

    image brightness check

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { ReactDOM } from "react";
-import productimg from "../assets/Products.jpg";
-
+import productimg from "../assets/Products-min.jpg";
+import turmericpowder from "../assets/Turmeric Powder.jpg";
+import blackturmeric from "../assets/black.jpg";
+import finger from "../assets/finger.jpg";
 const Products = () => {
   return (
     <>
@@ -11,10 +13,10 @@ const Products = () => {
           Our Products{" "}
         </h1>
 
-        <div class=" mt-10 grid md:grid-cols-2 md:gap-7 md:content-between  w-full ">
+        <div class=" mt-10 lg:mt-20 grid md:grid-cols-2 md:gap-7 md:content-between  w-full ">
           <img src={productimg} class="w-full rounded-lg shadow-lg " />
 
-          <span class="w-full mt-10 md:mt-0 space-y-2 text-sm md:text-sm font-medium text-slate-600 ">
+          <span class="w-full text-center sm:text-left mt-10 md:mt-0 space-y-2 text-sm md:text-sm lg:text-lg font-medium text-slate-500 ">
             {" "}
             <p>
               {" "}
@@ -35,6 +37,33 @@ const Products = () => {
               seriously.
             </p>
           </span>
+        </div>
+        <hr class="mt-10"></hr>
+        <h2 class="mt-10 text-2xl font-bold text-center text-slate-700">
+          Types of Turmeric we ship
+        </h2>
+
+        <div class="grid gap-4 mt-10 sm:grid-cols-3">
+          <div>
+            <div className=" relative w-full h-60 sm:h-full border-2 rounded-xl overflow-hidden  shadow-lg ">
+              <img class=" brightness-50 " src={turmericpowder} className="" />
+              <h1 className=" absolute text-center text-white inset top-1/2 left-1/2 text-3xl md:text-2xl lg:text-4xl font-bold -translate-x-1/2 -translate-y-1/2 ">
+                Turmeric Powder
+              </h1>
+            </div>
+          </div>
+          <div className=" relative w-full h-60 sm:h-full border-2 rounded-xl overflow-hidden  shadow-lg ">
+            <img class=" brightness-50 " src={blackturmeric} className="" />
+            <h1 className=" absolute text-center text-white inset top-1/2 left-1/2 text-3xl md:text-2xl lg:text-4xl font-bold -translate-x-1/2 -translate-y-1/2 ">
+              Black Turmeric{" "}
+            </h1>
+          </div>
+          <div className=" relative w-full h-60 sm:h-full border-2 rounded-xl overflow-hidden  shadow-lg ">
+            <img class=" brightness-50" src={finger} className="" />
+            <h1 className=" absolute text-center text-white inset top-1/2 left-1/2 text-3xl md:text-2xl lg:text-4xl font-bold -translate-x-1/2 -translate-y-1/2 ">
+              Turmeric Fingers{" "}
+            </h1>
+          </div>
         </div>
       </div>
     </>
