22fc08e0149e8dbc62f3a9eb8754090683b4d726
subodh Galande <subodh.dev@outlook.com>
2022-09-24 21:59:29 +0530

    testimonials added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,6 +12,9 @@ import tree from "../assets/tree.svg";
 import quality from "../assets/quality.svg";
 import supply from "../assets/supply.svg";
 import why from "../assets/why.png";
+import face1 from "../assets/face1.png";
+import face2 from "../assets/face2.png";
+import face3 from "../assets/face3.png";
 
 import Hero from "./hero";
 
@@ -95,7 +98,57 @@ const Home = () => {
           </div>
         </div>
       </section>
-
+      <section className="mx-auto mt-16 lg:mt-20 px-4 md:px-8 lg:px-12">
+        <h1 className="text-4xl lg:text-6xl text-center font-serif text-slate-600 font-black  ">
+          {" "}
+          What our clients say{" "}
+        </h1>
+        <div className="grid sm:grid-cols-3 mx-auto lg:w-5/6 gap-6 mt-16 w-full ">
+          <div class="rounded-lg shadow-md border py-10 bg-gray-50 ">
+            <div className=" ">
+              <img src={face1} alt="" class="h-16 px-8 sm:px-6 lg:px-8 " />
+              <p class="w-5/6 mx-auto pt-6  font-normal text-base text-slate-800">
+                “We sincerely appreciate your responsiveness and the way you
+                conduct business. We are highly please with your company because
+                of our satisfaction with your product supplies. We look forward
+                to doing business with you for years to come.”
+              </p>
+              <p class="w-5/6 mx-auto pt-6 text-base font-bold text-slate-500">
+                - Reputed Buyer from Canada
+              </p>
+            </div>
+          </div>
+          <div class="rounded-lg shadow-md border py-10 bg-gray-50 ">
+            <div className=" ">
+              <img src={face2} alt="" class="h-16 px-8  sm:px-6 lg:px-8  " />
+              <p class="w-5/6 mx-auto pt-6  font-normal text-base text-slate-800">
+                “In our business we must get our products to our warehouses on a
+                regular schedule. Thank you for your timely deliveries of
+                ordered products. We rely on dependable service from suppliers
+                like SBM Overseas to help us keep our schedule and satisfy our
+                customers.”
+              </p>
+              <p class="w-5/6 mx-auto pt-6 text-base font-bold text-slate-500">
+                - Reputed Buyer from USA
+              </p>
+            </div>
+          </div>{" "}
+          <div class="rounded-lg shadow-md border py-10 bg-gray-50 ">
+            <div className=" ">
+              <img src={face3} alt="" class="h-16 px-8  sm:px-6 lg:px-8  " />
+              <p class="w-5/6 mx-auto pt-6  font-normal text-base text-slate-800">
+                “We have worked with SBM Overseas for a sufficient time and
+                found them to be a great business partner. They are very keenly
+                focused on our needs and are always flexible in meeting our
+                requirements.”
+              </p>
+              <p class="w-5/6 mx-auto pt-6 text-base font-bold text-slate-500">
+                - Reputed Buyer from Germany
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
       <h2 className=" mt-20 lg:mt-32 font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-blue-600 via-blue-400 to-blue-600  lg:text-6xl font-sans md:text-5xl text-center">
         {" "}
         Certifications{" "}
