b154fec5aa80ffe60486b3b816f265a42a04db5c
subodh Galande <subodh.dev@outlook.com>
2022-09-25 15:33:32 +0530

    footer redesigned

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,19 +5,35 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <>
-      <footer class="text-gray-600 body-font mt-20 border-t-2">
-        <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            <img src={sbm} className="h-10" alt="" />
-          </a>
-          <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            © 2022 SBM Overseas | All rights reserved
-          </p>
-          <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center font-medium space-x-4 sm:justify-start">
-            <Link to="/">Home</Link>
-            <Link to="/Products">Products</Link>
-            <Link to="/Contact">Contact Us</Link>
-          </span>
+      <footer class=" mt-20 border-t-2">
+        <div class=" mx-auto flex flex-col sm:flex-row sm:justify-between px-4 max-w-7xl ">
+          <div className="mt-10 sm:w-1/2 lg:w-1/3">
+            <img
+              src={sbm}
+              className="h-12 sm:h-16 lg:h-20 mx-auto sm:mx-0 "
+              alt=""
+            />
+            <p className="text-sm sm:text-left text-center lg:text-lg text-slate-800 pt-3 lg:pt-5">
+              We are dedicated and one stop solution for the exportation and
+              marketing of pure quality Turmeric around the Globe.{" "}
+            </p>
+            <p className="text-sm sm:text-left text-center font-medium text-slate-800 pt-3 lg:text-lg ">
+              Copyright © 2022 SBM Overseas
+            </p>
+          </div>
+          <div className=" mt-5 sm:mt-10 space-x-4 sm:space-x-0 font-medium sm:flex lg:text-lg sm:flex-col text-slate-500 mx-auto sm:mx-0">
+            <Link>Home</Link>
+            <Link>About</Link>
+            <Link>Products</Link>
+            <Link>Contact Us</Link>
+          </div>
+          <div className=" my-5 sm:mt-10  mx-auto sm:mx-0">
+            <p className="text-center sm:text-right lg:text-lg text-slate-900 text-md">
+              To know more, drop a line at <br /> <b>info@sbmoverseas.com</b>{" "}
+              <hr class="my-2" /> Or call us on <br />
+              <b> +91 74151 27542</b>
+            </p>
+          </div>
         </div>
       </footer>
     </>
